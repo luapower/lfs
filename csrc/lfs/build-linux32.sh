37420f9269a4659480678d7632763d5bc1170b79
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:18 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/lfs/build-linux32.sh
+++ csrc/lfs/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc lfs.c -O3 -s -shared -o ../../bin/linux32/lfs.so -I../lua
+gcc $CFLAGS lfs.c -shared -o ../../bin/linux32/lfs.so -I../lua
 
 #../../linux/bin/luajit lfs_test.lua
--- csrc/lfs/build-mingw32.sh
+++ csrc/lfs/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc lfs.c -O3 -s -shared -o ../../bin/mingw32/clib/lfs.dll -I../lua -L../../bin/mingw32 -llua51
+gcc $CFLAGS lfs.c -shared -o ../../bin/mingw32/clib/lfs.dll -I../lua -L../../bin/mingw32 -llua51
 
 #../../bin/luajit lfs_test.lua
