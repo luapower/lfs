2e7e17d702f9079ac2350438db6c7254110caae0
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:51:21 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/lfs/build-linux32.sh
+++ csrc/lfs/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS lfs.c -shared -o ../../bin/linux32/clib/lfs.so -I../lua
+gcc -O2 -s -static-libgcc lfs.c -shared -o ../../bin/linux32/clib/lfs.so -I../lua
 
 #../../linux/bin/luajit lfs_test.lua
