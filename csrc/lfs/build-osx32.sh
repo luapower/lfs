cfa077398a1b2d8e9e68618d9adc10f253162f12
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:36:09 +0200

    osx32 build

---
+++ csrc/lfs/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 lfs.c -shared -o ../../bin/osx32/clib/lfs.so -I../lua -undefined dynamic_lookup
--- csrc/lfs/build-osx64.sh
+++ csrc/lfs/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 lfs.c -shared -o ../../bin/osx64/clib/lfs.so -I../lua -undefined dynamic_lookup
+gcc -arch x86_64 -O2 lfs.c -shared -o ../../bin/osx64/clib/lfs.so -I../lua -undefined dynamic_lookup
